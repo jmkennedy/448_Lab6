b5dd55671d9d5dd1e0f66ec01107dc8cb4ed3286
jmkennedy <jmkennedy94@gmail.com>
2015-03-26 13:43:04 -0500

    Slightly changed syntax

--- checkMatrix.cpp
+++ checkMatrix.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-bool checkMatrix(vector<vector<int>> mat) {
+bool checkMatrix(vector<vector<int> > mat) {
 	for (int i=0; i < mat.size - 1; i++) {
 		if(mat[i].size != mat[i+1].size) {return false;}
 	};
