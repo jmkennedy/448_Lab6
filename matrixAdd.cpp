ce3e4ecdd2f08df732c11c4a8f5b97c362d8d3c6
JMetcalfe201 <jmetcalfe201@gmail.com>
2015-03-26 13:45:23 -0500

    Added documentation

--- matrixAdd.cpp
+++ matrixAdd.cpp
@@ -6,16 +6,22 @@
 
 using namespace std;
 
+///////////////////////////
+// m1: the first matrix to be added
+// m2: the second matrix to be added
+// return: a matrix that is the result of the addition
 vector<vector<int>> matrixAdd(vector<vector<int> > m1, vector<vector<int> > m2)
 {
 	vector<vector<int> > result = {{0}};
 
+	//make sure they are the same size
 	if((m1.size() != m2.size()) || (m1[0].size() != m2[0].size()))
 	{
 		cout << "Matrixes are not the same size. Matrixes not added" << endl;
 		return result;
 	}
 
+	//Resize the result matrix
 	result.resize(m1.size());
 	
 	for(int i = 0; i < result.size(); i++)
@@ -23,6 +29,7 @@ vector<vector<int>> matrixAdd(vector<vector<int> > m1, vector<vector<int> > m2)
 		result[i].resize(m1[0].size());
 	}
 
+	//Assign the values
 	for(int i = 0; i < result.size(); i++)
 	{
 		for(int j = 0; j < result[0].size(); j++)
@@ -31,6 +38,7 @@ vector<vector<int>> matrixAdd(vector<vector<int> > m1, vector<vector<int> > m2)
 		}
 	}
 
+	//Return the result
 	return result;
 }
 
