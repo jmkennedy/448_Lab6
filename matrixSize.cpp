fda7a971d0b6ff319cd1da75ea73828b0ef2b700
John Metcalfe <jwmetcal@1005-17.eecs.ku.edu>
2015-03-31 13:42:22 -0500

    Changed conflicting function name and fixed size() calls

--- matrixSize.cpp
+++ matrixSize.cpp
@@ -1,9 +1,11 @@
 #include <vector>
 
-bool checkMatrix(vector<vector<int> > mat1, vector<vector<int> > mat2, int op){
+using namespace std;
+
+bool matrixSize(vector<vector<int> > mat1, vector<vector<int> > mat2, int op){
 	if (op == 1){//Addition
-		//Matricies must be same size
-		if(mat1[1].size == mat2[1].size && mat1.size == mat2.size){
+		//Matricies must be same size()
+		if(mat1[1].size() == mat2[1].size() && mat1.size() == mat2.size()){
 			//Checks dimensions
 			return true;
 		}else{
@@ -11,14 +13,14 @@ bool checkMatrix(vector<vector<int> > mat1, vector<vector<int> > mat2, int op){
 		}
 	}else if(op == 2){//Multiplication
 		//1st matrix is 2*3, second is 3*4
-		if(mat1.size == 2 && mat1[1].size == 3 && mat2.size == 3 && mat2[1].size == 4){
+		if(mat1.size() == 2 && mat1[1].size() == 3 && mat2.size() == 3 && mat2[1].size() == 4){
 			return true;
-		}else
+		}else{
 			return false;
 		}
 	}else if(op == 3){//transpose
 		//Only uses 1 matrix, is 2*3
-		if(mat1.size == 2 && mat1[1].size == 3){
+		if(mat1.size() == 2 && mat1[1].size() == 3){
 			return true;
 		}else{
 			return false;
@@ -27,3 +29,5 @@ bool checkMatrix(vector<vector<int> > mat1, vector<vector<int> > mat2, int op){
 		return false;
 	}
 }
+
+//shit
